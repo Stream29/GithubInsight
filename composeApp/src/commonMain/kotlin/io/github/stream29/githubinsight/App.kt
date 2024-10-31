fd7ce982757edb19e236a464ea55b93b30e7e6c2
Yume <2839681263@qq.com>
2024-10-31 14:31:34 +0800

    feat: 修改组件布局

--- composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/App.kt
@@ -1,27 +1,62 @@
 package io.github.stream29.githubinsight
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationDefaults
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import io.github.githubinsight.ui.common.Form
+import io.github.stream29.githubinsight.ui.card.InputKeyCard
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    val formMap = remember { mutableStateMapOf("api key" to "", "secret key" to "") }
+    val userMap = remember { mutableStateMapOf("user-homepage-address" to "") }
+    val keyMap = remember { mutableStateMapOf("api key" to "") }
+    var selectedItem by remember { mutableStateOf(0) }
     MaterialTheme {
-        Column(
-            modifier = Modifier.padding(10.dp)
+        Row(
+            modifier = Modifier.fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Form(formMap)
-            Text(formMap.toMap().toString())
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                InputKeyCard(keyMap)
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxHeight(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            BottomNavigation(
+                windowInsets = BottomNavigationDefaults.windowInsets
+            ) {
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Edit, contentDescription = null) },
+                    label = { Text("Test") },
+                    selected = selectedItem == 0,
+                    onClick = { selectedItem = 0 }
+                )
+            }
         }
     }
 }
