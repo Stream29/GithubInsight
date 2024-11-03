013f9d7bbf3e158b073165d6b3b3c6df3379b736
Yume <2839681263@qq.com>
2024-11-04 05:38:56 +0800

    feat(composeApp): change App layout

--- composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/App.kt
@@ -1,24 +1,13 @@
 package io.github.stream29.githubinsight
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationDefaults
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import io.github.stream29.githubinsight.ui.card.InputKeyCard
@@ -28,8 +17,9 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     val userMap = remember { mutableStateMapOf("user-homepage-address" to "") }
-    val keyMap = remember { mutableStateMapOf("api key" to "") }
-    var selectedItem by remember { mutableStateOf(0) }
+    val keyMap = remember { mutableStateMapOf<String, String>() }
+    val keyList = arrayOf("baidu", "gemini")
+//    var selectedItem by remember { mutableStateOf(0) }
     MaterialTheme {
         Row(
             modifier = Modifier.fillMaxHeight(),
@@ -39,24 +29,25 @@ fun App() {
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                InputKeyCard(keyMap)
+//                InputPathCard(userMap)
+                InputKeyCard(keyMap, keyList)
             }
         }
-        Row(
-            modifier = Modifier.fillMaxHeight(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.Bottom
-        ) {
-            BottomNavigation(
-                windowInsets = BottomNavigationDefaults.windowInsets
-            ) {
-                BottomNavigationItem(
-                    icon = { Icon(Icons.Filled.Edit, contentDescription = null) },
-                    label = { Text("Test") },
-                    selected = selectedItem == 0,
-                    onClick = { selectedItem = 0 }
-                )
-            }
-        }
+//        Row(
+//            modifier = Modifier.fillMaxHeight(),
+//            horizontalArrangement = Arrangement.Center,
+//            verticalAlignment = Alignment.Bottom
+//        ) {
+//            BottomNavigation(
+//                windowInsets = BottomNavigationDefaults.windowInsets
+//            ) {
+//                BottomNavigationItem(
+//                    icon = { Icon(Icons.Filled.Edit, contentDescription = null) },
+//                    label = { Text("Test") },
+//                    selected = selectedItem == 0,
+//                    onClick = { selectedItem = 0 }
+//                )
+//            }
+//        }
     }
 }
