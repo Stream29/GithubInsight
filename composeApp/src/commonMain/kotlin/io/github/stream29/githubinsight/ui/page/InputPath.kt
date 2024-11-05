282477f0600c093f08584186d8dae23f370732dd
Yume <2839681263@qq.com>
2024-11-06 00:01:23 +0800

    refactor(composeApp): change app layout

--- composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/ui/page/InputPath.kt
+++ composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/ui/page/InputPath.kt
@@ -1,12 +1,28 @@
 package io.github.stream29.githubinsight.ui.page
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import io.github.stream29.githubinsight.ui.card.InputPathCard
 
 @Composable
 fun InputPath(onStateChange: () -> Unit) {
     val userMap = remember { mutableStateMapOf("user-homepage-address" to "") }
-    InputPathCard(userMap, onStateChange)
+    Row(
+        modifier = Modifier.fillMaxHeight(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            InputPathCard(userMap, onStateChange)
+        }
+    }
 }
