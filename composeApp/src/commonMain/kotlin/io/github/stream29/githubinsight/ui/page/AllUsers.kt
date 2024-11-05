b070bf752efaf7662bce971ab10867312b2a77d0
Yume <2839681263@qq.com>
2024-11-06 00:01:56 +0800

    feat(composeApp): add AllUsers page

---
+++ composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/ui/page/AllUsers.kt
@@ -0,0 +1,32 @@
+package io.github.stream29.githubinsight.ui.page
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import io.github.stream29.githubinsight.type.UserInfo
+import io.github.stream29.githubinsight.ui.card.UserInfoCard
+import io.github.stream29.githubinsight.ui.component.SearchBar
+import io.github.stream29.githubinsight.ui.component.SelectCountry
+
+@Composable
+fun AllUsers(onStateChange: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column(verticalArrangement = Arrangement.Center) {
+                SelectCountry()
+            }
+            SearchBar(modifier = Modifier.align(Alignment.CenterVertically))
+        }
+        LazyColumn {
+            for (i in 1..30) {
+                item {
+                    UserInfoCard(index = i.toString(), userInfo = UserInfo)
+                }
+            }
+        }
+    }
+}
