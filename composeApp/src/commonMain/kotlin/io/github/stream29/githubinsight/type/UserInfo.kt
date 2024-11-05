707f9927818d105f4abfc92b1c8d8ea900f25e7d
Yume <2839681263@qq.com>
2024-11-05 23:58:43 +0800

    feat(composeApp): add UserInfoCard

---
+++ composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/type/UserInfo.kt
@@ -0,0 +1,7 @@
+package io.github.stream29.githubinsight.type
+
+object UserInfo {
+    val name: String = "stream29"
+    val avatarUrl: String = "https://avatars.githubusercontent.com/u/16459786?v=4"
+    val bio: String = "I'm a software engineer"
+}
