056bf28cdab6ac1f0549947cd1a994319e7ccdfb
Yume <2839681263@qq.com>
2024-11-06 17:15:01 +0800

    feat(recursiveAnalysis): add Entities.kt (unfinished)

--- recursiveAnalysis/src/commonMain/kotlin/io/github/stream29/githubinsight/analysis/Entities.kt
+++ recursiveAnalysis/src/commonMain/kotlin/io/github/stream29/githubinsight/analysis/Entities.kt
@@ -1,2 +1,36 @@
 package io.github.stream29.githubinsight.analysis
 
+import io.github.stream29.githubinsight.Organization
+import io.github.stream29.githubinsight.Repository
+
+data class UserResult(
+    val info: UserInfo,
+    val talentRank: UserTalentRank,
+    val nation: UserNation,
+)
+
+data class UserAnalysisData(
+    val info: UserInfo,
+    val followers: List<UserInfo>,
+    val followings: List<UserInfo>,
+    val repository: Repository,
+)
+
+data class UserInfo(
+    val name: String,
+    val avatar: String,
+    val profile: String,
+    val email: String?,
+    val organization: Organization
+)
+
+data class UserTalentRank(
+    val contributionVector: ContributionVector,
+    val confidence: Double?
+)
+
+data class UserNation(
+    val isEstimate: Boolean,
+    val nation: String,
+    val confidence: Double?
+)
