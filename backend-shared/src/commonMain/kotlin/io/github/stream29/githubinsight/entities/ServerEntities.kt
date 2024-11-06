1cf8e018c989afb399d4aa1d2b166a79d672fdff
Yume <2839681263@qq.com>
2024-11-06 19:20:16 +0800

    feat(backend-shared): add ServerEntities.kt

---
+++ backend-shared/src/commonMain/kotlin/io/github/stream29/githubinsight/entities/ServerEntities.kt
@@ -0,0 +1,43 @@
+package io.github.stream29.githubinsight.entities
+
+import io.github.stream29.githubinsight.analysis.ContributionVector
+
+data class UserResult(
+    val login: String,
+    val talentRank: ContributionVector<String>,
+    val nation: Estimated<String>
+)
+
+data class UserInfo(
+    val login: String,
+    val name: String,
+    val avatarUrl: String,
+    val bio: String?,
+    val email: String?,
+    val organizations: List<String>?,
+    val followers: List<String>,
+    val followingUrl: List<String>,
+//    订阅的仓库或用户
+    val subscriptionsUrl: String,
+    val reposUrl: String,
+//    val events: List<UserEvent>?
+    val company: String?,
+    val blog: String?,
+    val location: String?,
+    val publicRepos: Int,
+    val publicGists: Int,
+    val followersAmount: Int,
+    val followingAmount: Int
+)
+
+//暂时不考虑，类型太多
+//data class UserEvent(
+//    val type: String,
+//    val repo: String,
+//    val payload: Payload
+//)
+
+data class Estimated<T>(
+    val belief: Int,
+    val value: T
+)
