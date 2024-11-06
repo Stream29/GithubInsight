d321b08b26cc95ccfef08ec4a6031e7236ccf96d
Yume <2839681263@qq.com>
2024-11-06 23:25:48 +0800

    feat(backend-shared): modify entities file

--- backend-shared/src/commonMain/kotlin/io/github/stream29/githubinsight/entities/ServerEntities.kt
+++ backend-shared/src/commonMain/kotlin/io/github/stream29/githubinsight/entities/ServerEntities.kt
@@ -23,7 +23,6 @@ data class UserInfo(
     val subscriptions: List<String>,
 //    存储仓库请求路径：{login}/{repoName}
     val repos: List<String>,
-//    val events: List<UserEvent>?
     val company: String?,
     val blog: String?,
     val location: String?,
@@ -33,12 +32,38 @@ data class UserInfo(
     val followingAmount: Int
 )
 
-//暂时不考虑，类型太多
-//data class UserEvent(
-//    val type: String,
-//    val repo: String,
-//    val payload: Payload
-//)
+@Serializable
+data class Repository(
+    val name: String,
+    val htmlUrl: String,
+    val description: String?,
+    val collaborators: List<String>,
+    val teams: List<String>,
+    val tags: List<String>,
+//    使用的编程语言以及使用次数
+    val languages: Map<String,Int>,
+    val stargazers: List<String>,
+    val contributors: List<String>,
+    val subscribers: List<String>,
+    val commits: UserCommit,
+    val stargazersCount: Int,
+    val watchersCount: Int,
+    val forksCount: Int,
+    val topics: List<String>
+)
+
+@Serializable
+data class Organization(
+    val login: String,
+    val members: List<String>,
+    val description: String?
+)
+
+@Serializable
+data class UserCommit(
+    val author: String,
+    val message: String,
+)
 
 @Serializable
 data class Estimated<T>(
