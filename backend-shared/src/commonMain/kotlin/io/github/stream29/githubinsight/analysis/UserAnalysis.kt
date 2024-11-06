580ce9c19c52961f65ccba816ad915a5db4168e8
Yume <2839681263@qq.com>
2024-11-06 19:19:17 +0800

    delete(backend-shared): delete plus operator overload in ContributionVector

--- backend-shared/src/commonMain/kotlin/io/github/stream29/githubinsight/analysis/UserAnalysis.kt
+++ backend-shared/src/commonMain/kotlin/io/github/stream29/githubinsight/analysis/UserAnalysis.kt
@@ -3,15 +3,6 @@ package io.github.stream29.githubinsight.analysis
 import kotlin.jvm.JvmInline
 
 @JvmInline
-value class ContributionVector(
-    val contributionMap: Map<String, Int>
+value class ContributionVector<T>(
+    val contributionMap: Map<String, T>
 )
-
-operator fun ContributionVector.plus(other: ContributionVector): ContributionVector {
-    val newMap = mutableMapOf<String, Int>()
-    newMap.putAll(contributionMap)
-    other.contributionMap.forEach { (k, v) ->
-        newMap[k] = newMap.getOrElse(k) { 0 } + v
-    }
-    return ContributionVector(newMap)
-}
