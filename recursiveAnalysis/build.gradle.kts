d1a2b88b7265a724f5e3499b8f00caef8057ad4a
Stream <1542763342@qq.com>
2024-11-06 16:04:37 +0800

    chore(recursiveAnalysis): edit targets

--- recursiveAnalysis/build.gradle.kts
+++ recursiveAnalysis/build.gradle.kts
@@ -1,16 +1,24 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
-    jvm()
-    js().browser()
+    jvm {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
             dependencies {
                 implementation(project(":githubSpider"))
+                implementation(libs.mongodb.driver.kotlin.coroutine)
                 implementation(libs.langchain4kt.core)
                 implementation(libs.langchain4kt.utils)
                 implementation(libs.kotlinx.coroutines.core)
