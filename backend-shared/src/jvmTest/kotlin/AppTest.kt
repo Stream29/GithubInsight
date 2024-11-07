70b6759f4baf2c11179b1b61f0fd117badf226a5
Stream <1542763342@qq.com>
2024-11-07 23:39:35 +0800

    feat(backend-shared): 添加导入数据库方式

--- backend-shared/src/jvmTest/kotlin/AppTest.kt
+++ backend-shared/src/jvmTest/kotlin/AppTest.kt
@@ -5,6 +5,7 @@ import io.github.stream29.githubinsight.analysis.analyseUser
 import io.github.stream29.githubinsight.fromYamlString
 import io.github.stream29.githubinsight.toSpider
 import io.github.stream29.githubinsight.toSwitchOnFailApiProvider
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.io.File
 import kotlin.test.Test
@@ -21,8 +22,14 @@ class AppTest {
         val githubApiProvider = backendConfig.toSpider(mongoDatabase)
         val analyser = Analyser(mongoDatabase, chatApiProvider, githubApiProvider)
         runBlocking {
-            println(analyser.analyseUser("yumeowo"))
-            println(analyser.analyseUser("Stream29"))
+            launch { println(analyser.analyseUser("yumeowo")) }
+            launch { println(analyser.analyseUser("Stream29")) }
+            launch { println(analyser.analyseUser("Moistrocic")) }
+            launch { println(analyser.analyseUser("Singlenine")) }
+            launch { println(analyser.analyseUser("questionviper")) }
+            launch { println(analyser.analyseUser("Maple-Yorkin")) }
+            launch { println(analyser.analyseUser("owensichen")) }
+            launch { println(analyser.analyseUser("jjuaien")) }
         }
     }
 }
