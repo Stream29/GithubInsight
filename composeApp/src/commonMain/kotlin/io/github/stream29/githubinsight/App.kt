a7f83160ef80cbb5f6407a1fb1cd5e5cb7a9732b
Yume <2839681263@qq.com>
2024-11-07 18:15:14 +0800

    feat(composeApp): add UserRadarChart

--- composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/stream29/githubinsight/App.kt
@@ -6,19 +6,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import io.github.koalaplot.sample.polar.UserRadarChart
 import io.github.stream29.githubinsight.ui.page.AllUsers
 import io.github.stream29.githubinsight.ui.page.InputUserLogin
 import io.github.stream29.githubinsight.ui.page.UserDetail
 
 @Composable
 fun App() {
-    var pageState by remember { mutableStateOf(0) }
+    var pageState by remember { mutableStateOf(-100) } //调试
     val globalUserLogin = remember { mutableStateOf("") }
     MaterialTheme {
         when (pageState) {
             0 -> InputUserLogin { pageState = 1 }
-            1 -> AllUsers (onStateChange = { pageState = 2 }, globalUserLogin )
-            2 -> UserDetail({ pageState = 1 }, globalUserLogin.value)
+            1 -> UserDetail({ pageState = 2 }, globalUserLogin)
+            2 -> AllUsers({ pageState = 3 }, globalUserLogin)
+            3 -> UserDetail({ pageState = 2 }, globalUserLogin)
+
+            else -> UserRadarChart(false, "User Radar Chart", user!!.userResult.talentRank)
         }
     }
 }
