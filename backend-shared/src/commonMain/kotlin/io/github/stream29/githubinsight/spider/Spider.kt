2d25b055edb8587eed4c5f770cb720a697f4afda
Stream <1542763342@qq.com>
2024-11-07 18:15:50 +0800

    perf(backend-shared): 提高数据抓取效率

--- backend-shared/src/commonMain/kotlin/io/github/stream29/githubinsight/spider/Spider.kt
+++ backend-shared/src/commonMain/kotlin/io/github/stream29/githubinsight/spider/Spider.kt
@@ -4,59 +4,68 @@ import io.github.stream29.githubinsight.common.entities.Organization
 import io.github.stream29.githubinsight.common.entities.Repository
 import io.github.stream29.githubinsight.common.entities.UserInfo
 import io.github.stream29.githubinsight.spider.utils.*
+import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 
 
-class Spider (
+class Spider(
     private val balancingApiProvider: BalancingApiProvider
 ) {
-    suspend fun getUserInfo(login: String): UserInfo {
+    suspend fun getUserInfo(login: String): UserInfo = coroutineScope {
         val userResponse = balancingApiProvider.execute { a -> a.fetchUser(login) }
-        val organizationsResponse = balancingApiProvider.execute { a -> a.fetchOrganizations(userResponse.organizationsUrl) }
-        val reposResponse = balancingApiProvider.execute { a -> a.fetchRepositories(userResponse.reposUrl) }
-        val subscriptionsResponse = balancingApiProvider.execute { a -> a.fetchSubscriptions(userResponse.subscriptionsUrl) }
-        val followersResponse = balancingApiProvider.execute { a -> a.fetchUsers(userResponse.followersUrl) }
-        val followingResponse = balancingApiProvider.execute { a -> a.fetchUsers(userResponse.followingUrl) }
-        return EntityProcessor.toUserInfo(
+        val organizationsResponse =
+            async { balancingApiProvider.execute { a -> a.fetchOrganizations(userResponse.organizationsUrl) } }
+        val reposResponse = async { balancingApiProvider.execute { a -> a.fetchRepositories(userResponse.reposUrl) } }
+        val subscriptionsResponse =
+            async { balancingApiProvider.execute { a -> a.fetchSubscriptions(userResponse.subscriptionsUrl) } }
+        val followersResponse = async { balancingApiProvider.execute { a -> a.fetchUsers(userResponse.followersUrl) } }
+        val followingResponse = async { balancingApiProvider.execute { a -> a.fetchUsers(userResponse.followingUrl) } }
+        return@coroutineScope EntityProcessor.toUserInfo(
             ResponseCollection(
                 userResponse = userResponse,
-                orgsResponse = organizationsResponse,
-                reposResponse = reposResponse,
-                subscriptionsResponse = subscriptionsResponse,
-                followersResponse = followersResponse,
-                followingResponse = followingResponse,
+                orgsResponse = organizationsResponse.await(),
+                reposResponse = reposResponse.await(),
+                subscriptionsResponse = subscriptionsResponse.await(),
+                followersResponse = followersResponse.await(),
+                followingResponse = followingResponse.await(),
             )
         )
     }
 
     // get a repository all information
-    suspend fun getRepository(repoFullName: String): Repository {
+    suspend fun getRepository(repoFullName: String): Repository = coroutineScope {
         val repositoryResponse = balancingApiProvider.execute { a ->
             a.fetchRepository("$RepoUrl/$repoFullName")
         }
-        val contributors = balancingApiProvider.execute { a -> a.fetchContributors(repositoryResponse.contributorsUrl) }
-        val languages = balancingApiProvider.execute { a -> a.fetchLanguages(repositoryResponse.languagesUrl) }
-        val subscribers = balancingApiProvider.execute { a -> a.fetchSubscribers(repositoryResponse.subscribersUrl) }
-        val collaborators = balancingApiProvider.execute { a -> a.fetchCollaborators(repositoryResponse.collaboratorsUrl) }
-        val commits = balancingApiProvider.execute { a -> a.fetchCommits(repositoryResponse.commitsUrl) }
-        val tags = balancingApiProvider.execute { a -> a.fetchTags(repositoryResponse.tagsUrl) }
-        val readme = balancingApiProvider.execute { a -> a.fetchReadme(repositoryResponse.url) }
-        return EntityProcessor.toRepository(
+        val contributors =
+            async { balancingApiProvider.execute { a -> a.fetchContributors(repositoryResponse.contributorsUrl) } }
+        val languages =
+            async { balancingApiProvider.execute { a -> a.fetchLanguages(repositoryResponse.languagesUrl) } }
+        val subscribers =
+            async { balancingApiProvider.execute { a -> a.fetchSubscribers(repositoryResponse.subscribersUrl) } }
+        val collaborators =
+            async { balancingApiProvider.execute { a -> a.fetchCollaborators(repositoryResponse.collaboratorsUrl) } }
+        val commits = async { balancingApiProvider.execute { a -> a.fetchCommits(repositoryResponse.commitsUrl) } }
+        val tags = async { balancingApiProvider.execute { a -> a.fetchTags(repositoryResponse.tagsUrl) } }
+        val readme = async { balancingApiProvider.execute { a -> a.fetchReadme(repositoryResponse.url) } }
+        return@coroutineScope EntityProcessor.toRepository(
             repositoryResponse = repositoryResponse,
-            contributors = contributors,
-            languages = languages,
-            subscribers = subscribers,
-            collaborators = collaborators,
-            commits = commits,
-            tags = tags,
-            readmeContent = readme?.content ?: "",
+            contributors = contributors.await(),
+            languages = languages.await(),
+            subscribers = subscribers.await(),
+            collaborators = collaborators.await(),
+            commits = commits.await(),
+            tags = tags.await(),
+            readmeContent = readme.await()?.content ?: "",
         )
     }
+
 
     // get organization information
     suspend fun getOrganization(login: String): Organization = coroutineScope {
         val organizationResponse = balancingApiProvider.execute { a -> a.fetchOrganization(login) }
-        val organizationMembers = balancingApiProvider.execute { a -> a.fetchOrgMembers(organizationResponse.membersUrl) }
+        val organizationMembers =
+            balancingApiProvider.execute { a -> a.fetchOrgMembers(organizationResponse.membersUrl) }
         EntityProcessor.toOrganization(
             organizationResponse = organizationResponse,
             members = organizationMembers,
